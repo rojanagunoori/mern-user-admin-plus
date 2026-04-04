a34d6db1ee2865495d921f0cfca82a44a4bcf3d9
rojanagunoori <nagunooriroja@gmail.com>
2026-04-04 21:53:23 +0530

    chnaging

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,3 +1,4 @@
-//export const BASE_URL="http://localhost:6010"
+//export const BASE_URL1 = "http://localhost:6010";
+//export const BASE_URL = "http://localhost:5000";
 
-export const BASE_URL="https://mern-user-admin-plus-backend.onrender.com"
+export const BASE_URL = "https://mern-user-admin-plus-backend.onrender.com";
